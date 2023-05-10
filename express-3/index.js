e7b847c5ad3a348686e3cdb52a54956344329ceb
Manas <singhmanas610@gmail.com>
2023-05-11 00:04:24 +0530

    MVC

---
+++ express-3/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const server = express();
+const productRouter = express.Router()
+
+server.use(express.json());
+server.use(express.static("public"));
+server.use('/',productRouter)
+
+const productController = require('./controller/Product')
+
+// CREATE POST /products     C R U D
+productRouter
+.post("/products", productController.createProducts)
+// READ GET /product
+.get("/products", productController.getAllProducts)
+// READ GET /product/:id
+.get("/products/:id", productController.getProduct)
+// UPDATE PUT /product/:id
+.put("/products/:id", productController.replaceProduct)
+// UPDATE PATCH /product/:id
+.patch("/products/:id", productController.updateProduct)
+// DELETE DELETE /product/:id
+.delete("/products/:id", productController.deleteProduct)
+
+server.listen(8080, () => {
+  console.log("server started");
+});
